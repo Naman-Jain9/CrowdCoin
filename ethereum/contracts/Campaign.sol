04550a13a85b723ce9738a8eabd8f3062194e2f7
Naman Jain <69211223+Naman-Jain9@users.noreply.github.com>
2021-08-30 14:27:37 +0530

    Updating compiler version

--- ethereum/contracts/Campaign.sol
+++ ethereum/contracts/Campaign.sol
@@ -1,113 +1,120 @@
-pragma solidity >=0.4.17 <0.9.0;
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.5.0 <0.9.0;
 
 contract CampaignFactory {
-    address[] public deployedCampaigns;
+    Campaign[] public deployedCampaigns;
 
-    function createCampaign(uint minimum,string name,string description,string image,uint target) public {
-        address newCampaign = new Campaign(minimum, msg.sender,name,description,image,target);
+    function createCampaign(uint256 minimum) public {
+        Campaign newCampaign = new Campaign(minimum, msg.sender);
         deployedCampaigns.push(newCampaign);
     }
 
-    function getDeployedCampaigns() public view returns (address[]) {
+    function getDeployedCampaigns() public view returns (Campaign[] memory) {
         return deployedCampaigns;
     }
 }
 
-
 contract Campaign {
-  struct Request {
-      string description;
-      uint value;
-      address recipient;
-      bool complete;
-      uint approvalCount;
-      mapping(address => bool) approvals;
-  }
-
-  Request[] public requests;
-  address public manager;
-  uint public minimunContribution;
-  string public CampaignName;
-  string public CampaignDescription;
-  string public imageUrl;
-  uint public targetToAchive;
-  address[] public contributers;
-  mapping(address => bool) public approvers;
-  uint public approversCount;
-  uint public profit;
-
-  modifier restricted() {
-      require(msg.sender == manager);
-      _;
-  }
-
-  function Campaign(uint minimun, address creator,string name,string description,string image,uint target) public {
-      manager = creator;
-      minimunContribution = minimun;
-      CampaignName=name;
-      CampaignDescription=description;
-      imageUrl=image;
-      targetToAchive=target;
-  }
-
-  function contibute() public payable {
-      require(msg.value > minimunContribution );
-
-      contributers.push(msg.sender);
-      approvers[msg.sender] = true;
-      approversCount++;
-  }
-
-  function createRequest(string description, uint value, address recipient) public restricted {
-      Request memory newRequest = Request({
-         description: description,
-         value: value,
-         recipient: recipient,
-         complete: false,
-         approvalCount: 0
-      });
-
-      requests.push(newRequest);
-  }
-
-  function approveRequest(uint index) public {
-      require(approvers[msg.sender]);
-      require(!requests[index].approvals[msg.sender]);
-
-      requests[index].approvals[msg.sender] = true;
-      requests[index].approvalCount++;
-  }
-
-  function finalizeRequest(uint index) public restricted{
-      require(requests[index].approvalCount > (approversCount / 2));
-      require(!requests[index].complete);
-
-      requests[index].recipient.transfer(requests[index].value);
-      requests[index].complete = true;
-
-  }
-
-  function getProfit(uint p) public restricted {
-      uint profitPer = p/contributers.length;
-      for(uint i=0;i<contributers.length;i++){
-          contributers[i].transfer(profitPer);
-      }
-  }
-    function getSummary() public view returns (uint,uint,uint,uint,address,string,string,string,uint) {
-        return(
-            minimunContribution,
-            this.balance,
-            requests.length,
-            approversCount,
-            manager,
-            CampaignName,
-            CampaignDescription,
-            imageUrl,
-            targetToAchive
-          );
+    struct Request {
+        string description;
+        uint256 value;
+        address payable recipient;
+        bool complete;
+        uint256 approvalCount;
+        mapping(address => bool) approvals;
     }
 
-    function getRequestsCount() public view returns (uint){
-        return requests.length;
+    address public manager;
+    uint256 public minimumContribution;
+    mapping(address => bool) public approvers;
+    uint256 public approversCount;
+
+       uint256 numRequests;
+    mapping(uint256 => Request) public requests;
+
+    constructor(uint256 minimum, address creator) {
+        manager = creator;
+        minimumContribution = minimum;
+    }
+
+    function contribute() public payable {
+        require(
+            msg.value >= minimumContribution,
+            "A minumum contribution is required."
+        );
+        approvers[msg.sender] = true;
+        approversCount++;
+    }
+
+    function createRequest(
+        string memory description,
+        uint256 value,
+        address payable recipient
+    ) public onlyManager {
+        Request storage r = requests[numRequests++];
+        r.description = description;
+        r.value = value;
+        r.recipient = recipient;
+        r.complete = false;
+        r.approvalCount = 0;
+    }
+
+    function approveRequest(uint256 index) public {
+        Request storage request = requests[index];
+        require(
+            approvers[msg.sender],
+            "Only contributors can approve a specific payment request"
+        );
+        require(
+            !request.approvals[msg.sender],
+            "You have already voted to approve this request"
+        );
+
+        request.approvals[msg.sender] = true;
+        request.approvalCount++;
+    }
+
+    function finalizeRequest(uint256 index) public onlyManager {
+        Request storage request = requests[index];
+        require(
+            request.approvalCount > (approversCount / 2),
+            "This request needs more approvals before it can be finalized"
+        );
+        require(!(request.complete), "This request has already been finalized");
+
+        request.recipient.transfer(request.value);
+        request.complete = true;
+    }
+
+    function getSummary()
+        public
+        view
+        returns (
+            uint256,
+            uint256,
+            uint256,
+            uint256,
+            address
+        )
+    {
+        return (
+            minimumContribution,
+            address(this).balance,
+            numRequests,
+            approversCount,
+            manager
+        );
+    }
+
+    function getRequestsCount() public view returns (uint256) {
+        return numRequests;
+    }
+
+    modifier onlyManager() {
+        require(
+            msg.sender == manager,
+            "Only the campaign manager can call this function."
+        );
+        _;
     }
 }
